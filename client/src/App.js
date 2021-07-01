bb1c2a6051de9329704f22cc3c0a3949a6b07e2a
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-07-01 10:07:59 -0700

    Made cosmetic changes to banner, navbar and the cart link & it's icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { Container, Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Home from "./components/pages/home";
 import Search from './components/search'
 import Cart from "./components/cart";
-import Header from './components/header'
+import Navbar  from './components/navbar'
 import Footer from './components/footer'
 import NoMatch from './components/pages/no-match'
 import ProductDetail from './components/pages/productdetail'
@@ -22,24 +23,34 @@ const App = () => {
     <Router>
       <main className="py-3">
         <Container>
-          <LinkContainer to="/">
-            <Nav.Link>
-              <h1>ASPIN SOCIETY</h1>
-            </Nav.Link>
-          </LinkContainer>
+          <div className="banner">
+            <LinkContainer to="/">
+              <Nav.Link>
+                <h1>ASPIN SOCIETY</h1>
+              </Nav.Link>
+            </LinkContainer>
 
-          <Search />
-          <Cart />
+            <Search />
+
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <FontAwesomeIcon icon="shopping-cart" />
+              </Nav.Link>
+            </LinkContainer>
+          </div>
         </Container>
 
-        <Header />
+        <Navbar />
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/collections/:id" component={ProductDetail} />
-          <Route path="/search" component={SearchResults} />
-          <Route component={NoMatch} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/collections/:id" component={ProductDetail} />
+            <Route path="/search" component={SearchResults} />
+            <Route path="/cart/:id?" component={Cart} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Container>
       </main>
 
       <Footer />
