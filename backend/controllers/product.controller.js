43215d75ae42a32ab0ddd57a965f8d1b7ac340cc
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-07-02 09:26:59 -0700

    Using controllers

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("express-async-handler");
+let Product = require("../models/product.model");
+
+const getProducts = asyncHandler(async (req, res) => {
+
+     const products = await Product.find({});
+
+     res.json(products);
+})
+
+
+const getProductbyId = asyncHandler(async (req, res) => {
+  
+   const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Sorry, product can't be found");
+  }
+});
+
+module.exports = {
+    getProducts, getProductbyId
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,12 @@
 const productRouter = require('express').Router()
-const asyncHandler = require('express-async-handler')
-let Product = require('../models/product.model')
+const {
+  getProducts,
+  getProductbyId,
+} = require("../controllers/product.controller");
 
 
-productRouter.get("/", asyncHandler(async (req, res) => {
+productRouter.route("/").get(getProducts);
 
-  const products = await Product.find({}) 
-
-  res.json(products);
-}));
-
-productRouter.get("/:id", asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-
-  if(product){
-      res.json(product);
-  } else {
-      res.status(404)
-      throw new Error("Sorry, product can't be found")
-  }
-}));
+productRouter.route("/:id").get(getProductbyId);
 
 module.exports = productRouter;
