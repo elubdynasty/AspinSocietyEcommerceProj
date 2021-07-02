cefeda768decd7c808508f993780a54810d5f359
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-07-02 06:37:51 -0700

    Cart outputs like subtotals, change of qty and adding trash icon

--- client/src/components/cart.js
+++ client/src/components/cart.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { addToCart } from "../actions/cartActions"
 import Message from './message'
@@ -24,10 +25,102 @@ const Cart = ({match, location, history}) => {
     } 
   }, [dispatch, productId, qty])
 
+  const removefromCartHandler = id => {
+    console.log('removed')
+
+  }
+
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
+  }
+
   return (
-    <div>
-      This is cart
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is currently empty
+            <Link to="/"> Go Back </Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+
+                  <Col md={2}>
+                    <Row>${item.price}</Row>
+                  </Col>
+
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countinStock).keys()].map((count) => (
+                        <option key={count + 1} value={count + 1}>
+                          {count + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removefromCartHandler(item.product)}
+                    >
+                      <FontAwesomeIcon icon="trash" />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.price * item.qty, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={() => checkoutHandler()}
+              >Checkout</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
--- client/src/helpers/icons.js
+++ client/src/helpers/icons.js
@@ -1,11 +1,9 @@
-import { faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faShoppingCart, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
-  return library.add(
-    faSearch, faShoppingCart
-  );
+  return library.add(faSearch, faShoppingCart, faTrash);
 };
 
 export default Icons;
