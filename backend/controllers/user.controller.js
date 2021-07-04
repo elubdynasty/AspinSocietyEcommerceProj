b731a165fb46a36324560bcc262dc642c37e88ae
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-07-03 20:56:31 -0700

    Registration of a user and password encryption

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -21,6 +21,37 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+const regUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if(userExists){
+    res.status(400).json({message: 'User already exists!'})
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(user) {
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: genToken(user._id),
+    });
+
+  } else {
+    res.status(400).json({message: 'Invalid user data'})
+  }
+
+});
+
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
@@ -39,5 +70,5 @@ const getUserProfile = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  authUser, getUserProfile
+  authUser, getUserProfile, regUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const userRouter = require("express").Router();
 
-const { authUser, getUserProfile } = require("../controllers/user.controller");
+const { authUser, getUserProfile, regUser } = require("../controllers/user.controller");
 const protect = require('../middleware/auth.middleware')
 
+userRouter.route("/users").post(regUser);
 userRouter.route("/users/login").post(authUser);
 userRouter.route("/users/profile").get(protect, getUserProfile);
 
