683e66f0d53e053d10143a434c8dc7f6cb613b49
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-07-08 23:29:46 -0700

    Preparation to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const cors = require('cors')
+const morgan = require("morgan");
 const connectDB = require('./config/db')
+const path = require('path')
 
 require('dotenv').config()
 const PORT = process.env.PORT || 5000;
@@ -15,16 +16,13 @@ connectDB()
 
 const app = express()
 
-app.use(cors({
-    origin: "http://127.0.0.1:5000",
-    credentials: true
-}))
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
 
 
 app.use("/api", [productRouter, userRouter, orderRouter]);
@@ -32,6 +30,20 @@ app.use("/api", [productRouter, userRouter, orderRouter]);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+
+} else {
+
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+
+}
 
 
 app.use(NotFound);
