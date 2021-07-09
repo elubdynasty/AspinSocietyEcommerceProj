5d18b1150ebae2fb5da512007f6cd4264fe65433
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-07-08 21:33:27 -0700

    Putting to new branch

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from './actions/userActions';
 
-import NavContainer from "./components/pages/navbar";
+
 import Home from "./components/pages/home";
 import Footer from "./components/pages/footer";
 import NoMatch from "./components/pages/no-match";
@@ -71,8 +71,7 @@ const App = () => {
             )}
           </div>
         </Container>
-
-        <NavContainer />
+        
 
         <Container>
           <Switch>
