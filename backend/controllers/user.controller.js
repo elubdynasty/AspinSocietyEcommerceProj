41dac4074633d2d5d99956adcbca2c73733661a3
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-08-01 20:18:58 -0700

    Get user by ID and update user endpoints from Admin

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -128,6 +128,42 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if(user){
+    res.json(user);
+  } else {
+     res.status(404);
+     throw new Error("User can't be found");
+  }
+  
+});
+
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin
+
+    
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
 
 module.exports = {
   authUser,
@@ -135,5 +171,7 @@ module.exports = {
   regUser,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,25 @@
 const userRouter = require("express").Router();
 
-const { authUser, getUserProfile, regUser, updateUserProfile, getUsers, deleteUser } = require("../controllers/user.controller");
+const { 
+    authUser, 
+    getUserProfile, 
+    regUser, 
+    updateUserProfile, 
+    getUsers, 
+    deleteUser, 
+    getUserById, 
+    updateUser 
+} = require("../controllers/user.controller");
+
 const {protect, admin} = require('../middleware/auth.middleware')
 
 userRouter.route("/users").post(regUser).get(protect, admin, getUsers);
 userRouter.route("/users/login").post(authUser);
 userRouter.route("/users/profile").get(protect, getUserProfile)
 .put(protect, updateUserProfile);
+
 userRouter.route("/users/:id").delete(protect, admin, deleteUser)
+.get(protect, admin, getUserById)
+.put(protect, admin, updateUser)
 
 module.exports = userRouter;
