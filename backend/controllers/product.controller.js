8385d94da0e057b5c040c20e3290b57ea356c332
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-08-23 14:20:35 -0700

    Create & Update Product endpoints

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -38,6 +38,51 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+const createProduct = asyncHandler(async (req, res) => {
+  
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "./images/sample.jpg",
+    category: "Sample category",
+    countinStock: 0,
+    description: 'Sample desc'
+  });
+
+  const createdProduct = await product.save()
+
+  res.status(201).json(createdProduct);
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+
+  const { name, price, category, description, image, countinStock } = req.body;
+
+  const product = await Product.findById(req.params.id)
+
+  if(product){
+
+    product.name = name
+    product.price = price
+    product.category = category;
+    product.description = description;
+    product.image = image;
+    product.countinStock = countinStock;
+
+    const updatedProduct = await product.save();
+
+    res.status(201).json(updatedProduct);
+
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+    
+  }
+  
+});
+
+
 module.exports = {
-    getProducts, getProductbyId, deleteProduct
+    getProducts, getProductbyId, deleteProduct, createProduct, updateProduct
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,14 +2,23 @@ const productRouter = require('express').Router()
 const {
   getProducts,
   getProductbyId,
-  deleteProduct
+  createProduct,
+  deleteProduct,
+  updateProduct
 } = require("../controllers/product.controller");
 
 const { protect, admin } = require("../middleware/auth.middleware");
 
 
-productRouter.route("/products").get(getProducts);
+productRouter
+  .route("/products")
+  .get(getProducts)
+  .post(protect, admin, createProduct);
 
-productRouter.route("/products/:id").get(getProductbyId).delete(protect, admin, deleteProduct);
+productRouter
+  .route("/products/:id")
+  .get(getProductbyId)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 module.exports = productRouter;
