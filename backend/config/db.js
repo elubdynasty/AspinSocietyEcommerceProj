1f0fa13a36674c47827b430f954bd93efb068afc
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-09-01 19:46:29 -0700

    New version of mongoose no longer need of dep warning options

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.ATLAS_URI, {
-          useNewUrlParser: true,
-          useCreateIndex: true,
-          useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(process.env.ATLAS_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
         
