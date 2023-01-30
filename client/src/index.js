bf83c3aebf1f9847668b4376912556716ed399ee
Leonard Luzon <leonard_luzon2000@yahoo.com>
2023-01-29 16:55:20 -0800

    Deleted vercel analytics on the codebase

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import store from './store'
 import './styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { inject } from '@vercel/analytics';
+
 
 
 
@@ -19,5 +19,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-inject();
+
 reportWebVitals();
