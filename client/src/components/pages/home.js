e2535049f18617a0d6f037b5f7b42f2903d66a7f
Leonard Luzon <leonard_luzon2000@yahoo.com>
2023-02-05 08:42:51 -0800

    Implemented a truthy statement to check if products is defined in prod

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -41,7 +41,7 @@ const Home = ({ match }) => {
         ) : (
           <>
             <Row>
-              {products.map((product) => (
+              {products && products.map((product) => (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                   <Product product={product} />
                 </Col>
