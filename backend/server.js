bbad3346a73d41ed1f6d3e5f89610cb90ba20e55
Leonard Luzon <leonard_luzon2000@yahoo.com>
2023-02-09 12:27:28 -0800

    Added postbuild for render, some changes on file path for prod. and map fixes

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
