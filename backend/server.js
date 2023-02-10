f43d1dae00735c34d7c93d4812dbfe1025f26066
Leonard Luzon <leonard_luzon2000@yahoo.com>
2023-02-10 00:08:41 -0800

    Added cors for prod

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require("morgan");
 const connectDB = require('./config/db')
 const path = require('path')
+const cors = require('cors')
 
 require('dotenv').config()
 const PORT = process.env.PORT || 5000;
@@ -24,7 +25,7 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json())
 
-
+app.use(cors())
 
 app.use("/api", [productRouter, userRouter, orderRouter, uploadRouter]);
 
